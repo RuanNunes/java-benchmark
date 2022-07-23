927656810777e34e35d7b1fccc54f0f573fc3f1e
Ruan Nunes <ruanschwanz@gmail.com>
2022-07-23 00:22:45 -0300

    fix versions frameworks (#2)

---
+++ benchmark-spring/src/test/java/ruan/nunes/benchmark/BenchmarkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ruan.nunes.benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchmarkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
