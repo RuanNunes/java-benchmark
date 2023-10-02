6eab8fca6df19dd079c8d952d52d8645885c9099
RuanNunes <ruanschwanz@gmail.com>
2023-10-01 22:36:15 -0300

    add new versions spring native

---
+++ spring/native/11/3.1.4/src/test/java/com/ruannunes314/TestApplication.java
@@ -0,0 +1,13 @@
+package com.ruannunes314;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestApplication.class).run(args);
+	}
+
+}
