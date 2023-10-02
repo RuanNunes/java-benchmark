453fdbe17013219bd33b879a7807905d9b59df0c
RuanNunes <ruanschwanz@gmail.com>
2023-10-01 23:35:20 -0300

    add versions quarkus

---
+++ quarkus/17/3.4.1/src/native-test/java/com/ruannunes/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.ruannunes;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
