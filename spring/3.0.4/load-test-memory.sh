12182a9ec1c21aa25d8aeaae2fb5f299786e76b2
RuanNunes <ruanschwanz@gmail.com>
2023-10-31 21:40:40 -0300

    fiap live jvm/graalvm

---
+++ spring/3.0.4/load-test-memory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# URL do endpoint
+url="http://localhost:8080/jvm/memory"
+
+# Número de solicitações a serem enviadas
+num_requests=10000
+
+# Número de solicitações a serem enviadas simultaneamente
+concurrency=100
+
+# Executa o teste de carga
+ab -n $num_requests -c $concurrency $url
